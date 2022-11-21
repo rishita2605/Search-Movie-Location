c3f3991f0e0241c1cc04dfa82fad6519bcaf6c1f
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2022-11-21 16:58:47 +0530

    feat: Add search button

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -2,6 +2,7 @@ import { React, useState, useEffect } from 'react'
 
 import Autocomplete from 'react-autocomplete'
 import PropTypes from 'prop-types'
+import SearchBtn from '../SearchBtn'
 
 // import './Search.css'
 
@@ -29,10 +30,12 @@ export default function Search ({ movies, searchVal, setSearchVal, location, set
    * To search when enter is pressed.
    * @param object event
    */
-  const searchOnEnter = (event) => {
+  const searchMovie = (event) => {
     if (event.key === 'Enter') {
-      console.log(event.target.value)
       setSearchVal(event.target.value)
+    }
+
+    if (event.type === 'click' || event.key === 'Enter') {
       const loc = []
       for (const m of movies) {
         if (m.title === searchVal) loc.push(m.locations)
@@ -43,48 +46,50 @@ export default function Search ({ movies, searchVal, setSearchVal, location, set
   /* ---------- Function Methods ---------- */
 
   /* ++++++++++ Function Render Method ++++++++++ */
-  return <div className = 'search'><Autocomplete
-  items = {movieTitle}
-  // What value from the items' object should be displayed in the dropdown.
-  getItemValue = { (item) => item.title }
+  return <div className = 'search'>
+    <Autocomplete
+    items = {movieTitle}
+    // What value from the items' object should be displayed in the dropdown.
+    getItemValue = { (item) => item.title }
 
-  /**
-   * Renders the dropdown item.
-   *
-   * @param item
-   * @param isHighlighted controls whether the item should be highlighted.
-   */
-  renderItem = { (item, isHighlighted) => {
-    return <div key={item.title}
-    style={ { background: isHighlighted ? '#bcf5bc' : 'white' } }>
-      {item.title}
-    </div>
-  } }
+    /**
+     * Renders the dropdown item.
+     *
+     * @param item
+     * @param isHighlighted controls whether the item should be highlighted.
+     */
+    renderItem = { (item, isHighlighted) => {
+      return <div key={item.title}
+      style={ { background: isHighlighted ? '#bcf5bc' : 'white' } }>
+        {item.title}
+      </div>
+    } }
 
-  /**
-   * The items should be matched even if the cases don't match.
-   * shouldItemRender is invoked for each item in items. The return value of this function
-   * determines whether a dropdown element needs to be displayed or not.
-   * Displaying items only when they're found in the list. All items are displayed by default.
-   *
-   * @param item item from list
-   * @param value value entered in input
-  */
-  shouldItemRender = { (item, value) => {
-    return item.title.toLowerCase().indexOf(value.toLowerCase()) > -1 && value.length >= 0
-  } }
+    /**
+     * The items should be matched even if the cases don't match.
+     * shouldItemRender is invoked for each item in items. The return value of this function
+     * determines whether a dropdown element needs to be displayed or not.
+     * Displaying items only when they're found in the list. All items are displayed by default.
+     *
+     * @param item item from list
+     * @param value value entered in input
+    */
+    shouldItemRender = { (item, value) => {
+      return item.title.toLowerCase().indexOf(value.toLowerCase()) > -1 && value.length >= 0
+    } }
 
-  value={searchVal}
-  onChange={ (event) => setSearchVal(event.target.value) }
-  onSelect={ (val) => setSearchVal(val) }
+    value={searchVal}
+    onChange={ (event) => setSearchVal(event.target.value) }
+    onSelect={ (val) => setSearchVal(val) }
 
-  // Using render input so that the onKeyUp event can be attached to it.
-  renderInput={
-    function (props) {
-      return <input {...props} onKeyUp={(evt) => { searchOnEnter(evt) }}/>
+    // Using render input so that the onKeyUp event can be attached to it.
+    renderInput={
+      function (props) {
+        return <input {...props} onKeyUp={(evt) => { searchMovie(evt) }}/>
+      }
     }
-  }
-  />
+    />
+    <SearchBtn searchMovie = { searchMovie }/>
   </div>
 }
 
