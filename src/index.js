fdd5b32d32916532bf068e94eaef1368d0355b0f
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2022-11-25 19:21:09 +0530

    refactor: Remove index.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './global.scss'
-import './index.css'
+
 import App from './App'
+import './global.scss'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
