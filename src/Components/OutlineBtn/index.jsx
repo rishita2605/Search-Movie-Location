f534f60ae47f4ee34cb9db9c76695cbe08726417
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2022-11-27 20:49:41 +0530

    feat: Add navigate button for mobile UI

--- src/Components/OutlineBtn/index.jsx
+++ src/Components/OutlineBtn/index.jsx
@@ -4,9 +4,14 @@ import PropTypes from 'prop-types'
 
 import './OutlineBtn.scss'
 
-export default function OutlineBtn ({ btnText, btnOnClick, Icon }) {
+export default function OutlineBtn ({ btnText, btnOnClick, Icon, ...props }) {
+  /* ++++++++++ Function Constants ++++++++++ */
+  const classNames = ['outline-btn', ...props.className.split(' ')] // to allow multiple classes to be added
+  /* ---------- Function Constants ---------- */
+
+  /* ++++++++++ Function Render Method ++++++++++ */
   return (
-  <button className="outline-btn" onClick={ btnOnClick }>
+  <button className={classNames.join(' ')} onClick={ btnOnClick }>
   <span className="outline-btn__text">{ btnText }</span>
     <div className="outline-btn__icon"><Icon /></div>
   </button>
@@ -17,5 +22,6 @@ OutlineBtn.displayName = 'OutlineBtn'
 OutlineBtn.propTypes = {
   btnText: PropTypes.string.isRequired,
   btnOnClick: PropTypes.func,
-  Icon: PropTypes.object
+  Icon: PropTypes.object,
+  className: PropTypes.string
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
+import { ReactComponent as DownArrow } from '../Icons/DownArrow.svg'
+import './HomePage.scss'
 import MovieCard from '../Components/MovieCard'
 import MovieMap from '../Components/MovieMap'
+import OutlineBtn from '../Components/OutlineBtn'
 import Search from '../Components/Search'
-import './HomePage.scss'
+import { ReactComponent as UpArrow } from '../Icons/UpArrow.svg'
 
 export default function HomePage () {
   /* ++++++++++ Function State ++++++++++ */
   const [searchVal, setSearchVal] = useState('') // Defining search value so that it can be accessed in Map component as well.
   const [movies, setMovies] = useState([])
   const [location, setLocation] = useState([])
-  const [zoom, setZoom] = useState(2)
+  const [zoom, setZoom] = useState(0.5)
+  const [currentIcon, setCurrentIcon] = useState(DownArrow)
+  const navRef = useRef(null)
   /* ---------- Function State ---------- */
 
   /* ++++++++++ Side Effects ++++++++++ */
@@ -20,6 +25,9 @@ export default function HomePage () {
       .then((data) => {
         setMovies(data)
       })
+
+    // To move to different sections on mobile on button click
+    if (window.screen.width <= 576) { window.location.replace('/#column-1') }
   }, [])
 
   /* ---------- Side Effects ---------- */
@@ -28,29 +36,44 @@ export default function HomePage () {
   /* ---------- Function Constants ---------- */
 
   /* ++++++++++ Function Methods ++++++++++ */
+  /**
+   * Navigation button for mobile UI
+   */
+  const navigateOnClick = () => {
+    // We are already in 1st section - need to go to next section
+    if (navRef.current.classList.contains('active')) {
+      window.location.replace('/#column-2')
+      setCurrentIcon(UpArrow)
+    } else {
+      window.location.replace('/#column-1')
+      setCurrentIcon(DownArrow)
+    }
+
+    navRef.current.classList.toggle('active')
+  }
   /* ---------- Function Methods ---------- */
 
   /* ++++++++++ Function Render Method ++++++++++ */
   return <div className='home-page container'>
-    <div className="container__column container__column--left">
-    <Search
-      movies={ movies }
-      searchVal = { searchVal } setSearchVal = { setSearchVal }
-      location = { location } setLocation = { setLocation } />
-    <MovieMap
-      location = { location }
-      setLocation = { setLocation }
-      zoom = { zoom }
-      setZoom = { setZoom }/>
+    <div className="container__column container__column--left active" id='column-1' ref={navRef}>
+      <Search
+        movies={ movies }
+        searchVal = { searchVal } setSearchVal = { setSearchVal }
+        location = { location } setLocation = { setLocation } />
+      <MovieMap
+        location = { location }
+        setLocation = { setLocation }
+        zoom = { zoom }
+        setZoom = { setZoom }/>
     </div>
-    <div className="container__column container__column--right">
-    <MovieCard
-      movies={ movies }
-      location={ location }
-      searchVal={ searchVal }
-      setZoom = { setZoom }/>
+    <div className="container__column container__column--right" id='column-2'>
+      <MovieCard
+        movies={ movies }
+        location={ location }
+        searchVal={ searchVal }
+        setZoom = { setZoom }/>
     </div>
-
+    <OutlineBtn btnText='' btnOnClick={navigateOnClick} Icon={ currentIcon } className='navigate-btn'/>
   </div>
 }
 
